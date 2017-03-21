6f9f414bf4380f9845a5e7b9fe2720a398b2dda7
Rafael Grilli <rafaelgrilli92@hotmail.com>
2017-03-21 22:15:12 +1100

    creating button component

--- src/components/uiElements/button.js
+++ src/components/uiElements/button.js
@@ -1,17 +1,20 @@
 import React, { PropTypes } from 'react';
 import Icon from 'components/popular/icon';
 
-const Button = ({children, color = 'default', iconLeft, iconRight, modifier, type, onClick}) => {
+const Button = ({children, color = 'default', disabled, iconLeft, iconRight, modifier, text, type = 'button', onClick}) => {
 	return (
-		<button type={type} onClick={onClick} className={`btn btn-${color} ${modifier}`} >
-		{ iconLeft && (<Icon />) }
+		<button type={type} disabled={disabled} onClick={onClick} className={`btn btn-${color} ${modifier}`} >
+		{ iconLeft && (<span><Icon name={iconLeft} />&nbsp;</span>) }
+		{ text }
 		{ children }
-		{ iconRight }
+		{ iconRight && (<span>&nbsp;<Icon name={iconRight} /></span>) }
 		</button>
 	);
 }
 Button.propTypes = {
-  color: PropTypes.string, // default, green, yellow, red
+  color: PropTypes.string, // default, primary, danger, warning
+  iconLeft: PropTypes.string,
+  iconRight: PropTypes.string,
   modifier: PropTypes.string,
   onClick: PropTypes.func,
   type: PropTypes.string,
