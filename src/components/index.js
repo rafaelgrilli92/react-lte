5e9b038a9f54c93b29850da1311bb07a9280a42e
Rafael Grilli <rafaelgrilli92@hotmail.com>
2017-04-11 22:36:28 +1000

    changing Callout path

--- src/components/index.js
+++ src/components/index.js
@@ -5,10 +5,10 @@ export { default as Navbar } from  './layout/Navbar';
 export { default as SideBar, SideBarMenu, SideBarSubMenu, SideBarItem } from  './layout/SideBar';
 
 // UI Elements
+export { default as Callout } from  './uiElements/Callout';
 export { default as Icon } from  './uiElements/Icon';
 
 // Widgets
 export { default as Box } from './widgets/Box';
-export { default as Callout } from  './widgets/Callout';
 export { default as InfoBox } from './widgets/InfoBox';
 export { default as SmallBox } from  './widgets/SmallBox';
